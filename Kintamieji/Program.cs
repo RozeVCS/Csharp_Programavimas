88622e7a39b10107fa61ddc847cf95531874f0c0
Roze Rekute <roze.gelyte@outlook.com>
2023-10-22 15:00:46 +0300

    Add project files.

---
+++ Kintamieji/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace Kintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sveikieji
+            var amzius = 15;
+            int skaicius = 10;
+            int naujasSkaicius = amzius;
+
+            //Trupmeniniai
+            float ugis = 1.91f; //metrai
+            float ugisBeKablelio = 191f; //centimetrai
+            double ugisTikslesnis = 1.123456789f;
+
+            //Simboliniai
+            char simbolis = '@';
+            string tekstas = "Šiandien yra spalio 11 diena";
+            string tuscias = "";
+
+            //Loginiai
+            bool arLyja = false;
+            bool arTamsu = true;
+
+            // Kintamieji reikšmių keitimas
+            string naujasTekstas = "Labas";
+            naujasTekstas = "Ate";
+            naujasTekstas = "Viso gero";
+
+            // Kintamieji be reikšmių
+            int skaiciusBeReiksmesSveikasis; // 0
+            float skaiciusBeReiksmesTrumpmeninis; // 0
+            char simbolisBeReiksmes;
+            string tekstasBeReiksmes;
+
+        }
+    }
+}
