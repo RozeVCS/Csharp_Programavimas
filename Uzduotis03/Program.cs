0104eda076bb9e886086c5919a4446816a49c325
Roze Rekute <roze.gelyte@outlook.com>
2023-10-28 20:38:15 +0300

    All changes saved

---
+++ Uzduotis03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Uzduotis03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Išveskite į ekraną norimo dydžio tuščiavidurį kvadratą iš žvaigždučių.
+            int size = 5; // Change the size to your desired value
+
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= size; j++)
+                {
+                    if (i == 1 || i == size || j == 1 || j == size)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+
+            }
+    }
+    }
+
+}
+
---
+++ Uzduotis04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uzduotis04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Susikurkite reikiamus kintamuosius užduočiai atlikti. 
+            /*Į ekraną išveskite: Automobilio {markė} 
+             * {modelis}
+             * ({metai} m.) 
+             * rida - {rida} km.
+            Tarp {} nurodyti reikiami kintamieji,
+            šiose vietose jų reikšmės turi būti įstatytos.
+            */
+            string markė = "Toyota";
+            string modelis = "Camry";
+            int metai = 2015;
+            double rida = 41000.5;
+
+            Console.WriteLine($"Automobilio markė: {"Toyota"}, modelis {"Camry"}, metai ({2015} m.), rida {41000.5} km");
+
+        }
+    }
+}
