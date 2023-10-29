04915456acae5f70f724199a5a6ee289d299145f
Roze Rekute <roze.gelyte@outlook.com>
2023-10-29 13:48:45 +0200

    Add 27 task as completed

---
+++ Uzduotis27/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uzduotis27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Susikurti žodžių sąrašą. Rasti trumpiausią ir ilgiausią žodžius,
+            // juos išvesti į  ekraną,
+            // prie kiekvieno pasakant iš kiek raidžių jis yra sudarytas.
+            //Tip: string kintamasis iš tiesų yra char masyvas, todėl galite naudoti
+            //zodis.Length
+
+            List<string> words = new List<string>
+        {
+            "milk",
+            "juice",
+            "vodka",
+            "wine",
+            "gin tonic",
+            "whiskey",
+        };
+
+            string shortestWord = words[0];
+            string longestWord = words[0];
+
+            foreach (string word in words)
+            {
+                if (word.Length < shortestWord.Length)
+                {
+                    shortestWord = word;
+                }
+
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+
+            Console.WriteLine("List of Words:");
+            foreach (string word in words)
+            {
+                Console.WriteLine($"{word} ({word.Length} letters)");
+            }
+
+            Console.WriteLine("Shortest Word: " + shortestWord + " (" + shortestWord.Length + " letters)");
+            Console.WriteLine("Longest Word: " + longestWord + " (" + longestWord.Length + " letters)");
+
+
+
+        }
+    }
+}
