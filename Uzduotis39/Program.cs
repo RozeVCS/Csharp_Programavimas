a87b69ec782b7685d8fdb4bb904b8ab2954cb620
Roze Rekute <roze.gelyte@outlook.com>
2023-10-29 15:44:38 +0200

    Add 39 task as completed

---
+++ Uzduotis39/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Uzduotis39
+{
+
+    // Susikurti naują projektą pavadinimu “Dienynas”
+    // Susikurti projekto klases:
+    // Pagrindinė klasė(su Main() metodu, klasės pavadinimas nėra svarbus)
+    //Pagal nutylėjimą bus sukurta klasė pavadinimu “Program”
+    //Dienynas
+    //Mokinys
+    //PamokosPazymiai
+
+    namespace Diary
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                
+            }
+        }
+
+        class Diary
+        {
+            
+        }
+
+        class Student
+        {
+            
+        }
+
+        class Lessons
+        {
+            
+        }
+
+        class Grades
+        {
+            
+        }
+    }
+}
+
