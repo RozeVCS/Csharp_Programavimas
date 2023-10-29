c49c8344fedd2a1c478b3436a580786f455dee2d
Roze Rekute <roze.gelyte@outlook.com>
2023-10-29 14:08:21 +0200

    Add 28 task as completed

---
+++ Uzduotis28/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uzduotis28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static void OutputText()
+        {
+            Console.WriteLine("Today is a very lovely day.");
+        }
+
+        private static void Main1()
+        {
+            // Call the custom method to output text
+            OutputText();
+        }
+    }
+}
