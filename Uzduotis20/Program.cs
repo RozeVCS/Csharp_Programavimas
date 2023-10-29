d1d9cde8089c285e2d81cf57daf4951dc1437567
Roze Rekute <roze.gelyte@outlook.com>
2023-10-29 12:13:17 +0200

    Add 20 task completed

---
+++ Uzduotis20/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uzduotis20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Įveskite iš konsolės 2 skaičius (stataus trikampio statinių ilgius).
+            Suraskite įžambinę ir ją išveskite į ekraną.
+            */
+
+            Console.Write("Enter the length of the first side: ");
+            double side1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the length of the second side: ");
+            double side2 = double.Parse(Console.ReadLine());
+
+            double hypotenuse = Math.Sqrt(Math.Pow(side1, 2) + Math.Pow(side2, 2));
+
+            Console.WriteLine($"The length of the hypotenuse (preamble) is: {hypotenuse:F2}");
+
+        }
+    }
+}
