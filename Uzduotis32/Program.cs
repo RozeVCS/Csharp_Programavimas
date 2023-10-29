da0dfbd4b5edda07b923ccd0bbf5a3af1a66a9af
Roze Rekute <roze.gelyte@outlook.com>
2023-10-29 14:58:07 +0200

    Add 32 task as completed

---
+++ Uzduotis32/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uzduotis32
+{
+    internal class Program
+    {
+
+        //Paprašyti įvesti vartotojo vardą.
+        // Sukurti string tipo metodą, kuris gražina frazę pvz „Sveiki, Mantas“
+        // Išvesti šią frazę į konsolę.
+
+        // Method that takes a username and returns a greeting
+        static string GetGreeting(string username)
+        {
+            return "Hello " + username;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter your username: ");
+            string username = Console.ReadLine();
+
+            string greeting = GetGreeting(username);
+
+            Console.WriteLine(greeting);
+        }
+
+    }
+}
